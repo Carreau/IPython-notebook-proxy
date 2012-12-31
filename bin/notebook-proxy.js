cd13c2585565606c12d8358c8433464fc218ca4e
Bussonnier Matthias <bussonniermatthias@gmail.com>
2012-12-31 19:44:10 +0100

    repl option

--- bin/notebook-proxy.js
+++ bin/notebook-proxy.js
@@ -4,6 +4,8 @@ var program = require('commander');
 program
   //.version('0.0.1') // get that from package.json
   .option('-p, --port <n>', 'The port to listen on', parseInt, 80)
+  .option('-r, --repl', 'Start an REPL to give interactive command')
   .parse(process.argv);
 
 console.log('you asked for port ', program.port);
+console.log('You want to start the repl :', program.repl);
