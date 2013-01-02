3af1c24071636ab8c73e19f391b5420497e34aa5
Bussonnier Matthias <bussonniermatthias@gmail.com>
2013-01-02 22:20:23 +0100

    start/stop from command line

--- bin/notebook-proxy.js
+++ bin/notebook-proxy.js
@@ -12,10 +12,12 @@ program
   .option('-r, --repl', 'Start an REPL to give interactive command')
   .parse(process.argv)
 
-console.log('you asked for port ', program.port)
-console.log('You want to start the repl :', program.repl)
+//console.log('you asked for port ', program.port)
+//console.log('You want to start the repl :', program.repl)
 
-if(program.repl == true){
+
+var help = function(){
+
   console.log('==================================')
   console.log('add remove routing rules like so :')
   console.log('add_rule("/myurl/",portnumber)')
@@ -30,10 +32,42 @@ if(program.repl == true){
   console.log('Example :')
   console.log('> add_rule("/ipython/",8888)')
   console.log('')
+  console.log('')
   console.log('==================================')
-  var r = repl.start();
+  console.log('Use the following to quickly spawn a server')
+  console.log('> start("/url/",port)')
+  console.log('')
+  console.log('stop all running server with ')
+  console.log('> stop_all()')
+  console.log('')
+  console.log('==================================')
+
+}
+
+if(program.repl == true || true){
+
+  console.log('==================================')
+  console.log(' QuickStart ')
+  console.log('')
+  console.log('> start("/ipython/",8888)')
+  console.log('go to "localhost:8000/ipython/"')
+  console.log('')
+  console.log('> start("/ipython-more/",7777')
+  console.log('go to "localhost:8000/ipython-more/"')
+  console.log('')
+  console.log('> stop_all()')
+  console.log('')
+  console.log('> help()')
+  console.log('For more info')
+  console.log('==================================')
+  var r = repl.start({});
 
   r.context.proxyServer = proxy.proxyServer;
   r.context.rem_rule = proxy.rem_rule;
   r.context.add_rule = proxy.add_rule
+  r.context.list = proxy.list
+  r.context.start = proxy.start
+  r.context.stop = proxy.stop
+  r.context.stop_all = proxy.stop_all
+  r.context.spawned = proxy.spawned
 }
